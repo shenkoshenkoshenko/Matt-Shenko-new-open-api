37621297456e5301e7f6dac8c88f80e46f2a84c8
shenkoshenkoshenko <shenkoshenkoshenko@gmail.com>
2024-10-02 17:33:37 -0400

    Final updates complete

--- js/stouts.js
+++ js/stouts.js
@@ -1,9 +1,6 @@
 const outputMessage = document.getElementById("beer-output");
-const beerListSection = document.getElementById("beers-box")
-
-
-
-
+const beerListSection = document.getElementById("beers-box");
+const wikiBeer = document.getElementById("about-beer");
 
 
 
@@ -47,3 +44,22 @@ async function getBeers() {
   }
 
   getBeers()
+
+
+
+  function searchBeer() {
+    // ***********************************this creates JS to send to wikipedia instead of an HTML request so as to get around CORS issue
+    const searchBeer = document.createElement('script');
+    searchBeer.src = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&srlimit=20&srsearch=stout&callback=wikiSearch&origin=*`;
+    document.body.appendChild(searchBeer);
+    // ***********************************the above apends the script to HTML, which loads the API call
+    //************************************wikipedia then sends back 'data' as an argument to the wikiSearch function, below */
+  }
+  
+  function wikiSearch(data) {
+    console.log(data);
+    wikiBeer.innerHTML = `${data.query.search[0].snippet}...\<a class="outside-link" href="https://en.wikipedia.org/wiki/Stout">(read more)</a>`
+
+  }
+  
+  searchBeer();
